0d79ac01a92d9389cceae28ec9b012f21671419e
Wang <wangtim27@7hills.org>
2025-10-17 09:09:22 -0400

    created battleship

---
+++ Battleship.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Battleship {
+	public String Start() {
+		System.out.println("Welcome, here are the rules to Battleship:\n");
+		System.out.println("Hunt, sink, and win with this brilliant Battleship game! Can you sink your opponents ships before all of yours go down? Command your battle and deploy your 4 ships on the ocean grid. The vertical target grid keeps you from seeing each others deployment! Keep firing and record your strikes with the red pegs. Use the white pegs to keep track of your misses so you dont waste any shots. If you use logic and take some chances, youll sink your opponents battleship  but do it fast, because theyll be trying to do the same to you! Rule the oceans with your Battleship game!
+
+Classic Battleship game includes 8 plastic ships, 60 red pegs and 140 white pegs
+
+Ocean base supports target grid
+
+Red and white pegs track hits and misses
+
+Sink your opponents battleships for the win
+
+Convenient ship and peg storage on the side of the ocean grid.
+
+Ages 7 and up
+
+For 2 players.");
+		System.out.println("You got that? Type Y for yes or N for no");
+		
